5b8c3de24029186f3a3ea717d937eb2d7c0b4bf0
Gene Marks <genemarks74@gmail.com>
2025-02-12 12:38:28 -0500

    update namespace to match folder structure

--- src/Netmash.Shared/Models/Style/CssPropertyValidator.cs
+++ src/Netmash.Shared/Models/Style/CssPropertyValidator.cs
@@ -1,6 +1,6 @@
 using Netmash.Shared.Interfaces;
 
-namespace Netmash.Shared.Models;
+namespace Netmash.Shared.Models.Style;
 
 public class CssPropertyValidator(HashSet<string> allowedCssProperties) : IStringValidator
 {
