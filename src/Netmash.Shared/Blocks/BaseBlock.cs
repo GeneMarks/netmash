1f324daabddb38148993c456c31072957b142da1
Gene Marks <genemarks74@gmail.com>
2025-03-26 22:58:40 -0400

    formatting

--- src/Netmash.Shared/Blocks/BaseBlock.cs
+++ src/Netmash.Shared/Blocks/BaseBlock.cs
@@ -7,10 +7,11 @@ namespace Netmash.Shared.Blocks;
 
 public abstract class BaseBlock : Entity, ISortable, IStylable
 {
-    public abstract BlockType BlockType { get; }
     public uint SortOrder { get; set; }
     public string DivId { get; set; } = IdGenerator.NewDivId();
     public HashSet<Style> Styles { get; set; } = [];
+
+    public abstract BlockType BlockType { get; }
 
     public virtual IEnumerable<IStylable> GetStylableChildren() => [];
 }
