821ae9c6ce501a41e96fffb36ebbaa5ed72eb0b4
Gene Marks <genemarks74@gmail.com>
2025-03-26 23:02:04 -0400

    add tests projects

---
+++ tests/Netmash.UnitTests/Shared/MashTests.cs
@@ -0,0 +1,17 @@
+﻿using Netmash.Shared.Mashes;
+
+namespace Netmash.UnitTests.Shared;
+
+public class MashTests
+{
+    [Fact]
+    public void Mash_Name_ShouldThrowExceptionOnEmpty()
+    {
+        var mash = new Mash("Test Name");
+
+        Assert.Throws<ArgumentException>(() =>
+        {
+            mash.Name = "";
+        });
+    }
+}
