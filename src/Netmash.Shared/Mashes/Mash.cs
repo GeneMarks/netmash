848c53b1581ef3ab821372f3871e2af4075b60de
Gene Marks <genemarks74@gmail.com>
2025-03-26 23:01:23 -0400

    introduce Name setter conditions

--- src/Netmash.Shared/Mashes/Mash.cs
+++ src/Netmash.Shared/Mashes/Mash.cs
@@ -6,11 +6,31 @@ using Netmash.Shared.Utilities;
 
 namespace Netmash.Shared.Mashes;
 
-public class Mash(string name) : Entity, IStylable
+public class Mash : Entity, IStylable
 {
     /*public Account Owner { get; }*/
-    public string Name { get; set; } = name;
+    private string _name;
+    public string Name
+    {
+        get => _name;
+        set => SetName(value);
+    }
     public List<BaseBlock> Blocks { get; set; } = [];
     public string DivId { get; set; } = IdGenerator.NewDivId();
     public HashSet<Style> Styles { get; set; } = [];
+
+    public Mash(string name)
+    {
+        SetName(name);
+    }
+
+    private void SetName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Name cannot be blank.");
+        if (name.Length > 100)
+            throw new ArgumentException("Name cannot exceed 100 characters.");
+
+        _name = name;
+    }
 }
