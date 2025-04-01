1c4cea0acf5cb301d33e32646f824d935107760d
Gene Marks <genemarks74@gmail.com>
2025-04-01 14:30:45 -0400

    add StylesConverter integration tests

--- tests/Netmash.IntegrationTests/Server/StylesConverterTests.cs
+++ tests/Netmash.IntegrationTests/Server/StylesConverterTests.cs
@@ -1,6 +1,31 @@
+using Netmash.Shared.Mashes;
+
 namespace Netmash.IntegrationTests.Server;
 
 public class StylesConverterTests
 {
+    [Fact]
+    public void Styles_AreSavedAndLoadedCorrectly()
+    {
+        using var context = TestDbContextFactory.Create();
 
+        var mash = new Mash("Stylable Mash")
+        {
+            Styles = [new(".stylable-mash", "color", "red")]
+        };
+
+        context.Mashes.Add(mash);
+        context.SaveChanges();
+
+        context.ChangeTracker.Clear(); // Stop tracking mash to ensure true db loading
+
+        var loadedMash = context.Mashes.First();
+
+        var expectedStyle = mash.Styles.First();
+        var loadedStyle = loadedMash.Styles.First();
+
+        Assert.Equal(expectedStyle.Selector, loadedStyle.Selector);
+        Assert.Equal(expectedStyle.Rule, loadedStyle.Rule);
+        Assert.Equal(expectedStyle.Value, loadedStyle.Value);
+    }
 }
