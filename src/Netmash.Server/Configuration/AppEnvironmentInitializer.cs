cef81982980ec9ebb8ea16c4fde00e65fa1bfaa1
Gene Marks <genemarks74@gmail.com>
2025-04-07 23:59:41 -0400

    Implement AppEnvironmentInitializer
    - Currently responsible for ensuring application folders are created
    if not present.

---
+++ src/Netmash.Server/Configuration/AppEnvironmentInitializer.cs
@@ -0,0 +1,22 @@
+using Serilog;
+
+namespace Netmash.Server.Configuration;
+
+public static class AppEnvironmentInitializer
+{
+    public static void InitializeDirectories(AppSettings settings)
+    {
+        Log.Information("Initializing app directories...");
+
+        foreach (var dir in new[]
+        {
+            settings.DbDirectory,
+            settings.LogDirectory,
+            settings.UploadDirectory
+        })
+        {
+            Log.Information("Ensuring directory exists: {Directory}", dir);
+            Directory.CreateDirectory(dir);
+        }
+    }
+}
