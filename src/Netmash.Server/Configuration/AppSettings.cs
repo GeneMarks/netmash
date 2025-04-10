4301fc9e7612fdc6468d11d805ce9eea0e8b78ea
Gene Marks <genemarks74@gmail.com>
2025-04-09 20:01:02 -0400

    update default Directory property paths to use storage folder

--- src/Netmash.Server/Configuration/AppSettings.cs
+++ src/Netmash.Server/Configuration/AppSettings.cs
@@ -2,8 +2,8 @@ namespace Netmash.Server.Configuration;
 
 public class AppSettings
 {
-    public string DbDirectory { get; set; } = "db";
+    public string DbDirectory { get; set; } = "storage/db";
     public string DbPath => Path.Combine(DbDirectory, "netmash.db");
-    public string LogDirectory { get; set; } = "logs";
-    public string UploadDirectory { get; set; } = "uploads";
+    public string LogDirectory { get; set; } = "storage/logs";
+    public string UploadDirectory { get; set; } = "storage/uploads";
 }
